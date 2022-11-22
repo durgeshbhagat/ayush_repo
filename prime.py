360a56084f43e2e3c38d9efe373f7a71c3da8fd3
Durgesh Kumar <durgeshit@gmail.com>
2022-11-22 17:41:35 +0530

    Changes the iteration count

--- prime.py
+++ prime.py
@@ -1,10 +1,11 @@
 # to check whther a number is prime or not
 n = int(input())
 is_prime = True
-for i in range(2,n):
+if n% 2 ==0:
+    is_prime = False
+for i in range(3,n,2):
     if n%i ==0:
         is_prime = False
-        break 
 if is_prime:
     print(f'{n} is prime')
 else:
